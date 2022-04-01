3e4542acfb6bd2ae84eea631ff3182f1270f786c
Júnior Moura <geraldomartins10@gmail.com>
2022-04-01 17:28:11 -0300

    Atividade do curso sobre Módulos

---
+++ main.mjs
@@ -0,0 +1,5 @@
+import {mostraIdade, mostraCidade, mostraHobby} from "./funcoes.mjs";
+
+console.log(mostraIdade("Júnior", 41));
+console.log(mostraCidade("Júnior", "Fortaleza"));
+console.log(mostraHobby("Júnior", "Cinema"));
